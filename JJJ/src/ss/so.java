1ddcfa73643b6e943164e6c78dae609fb6b2fb9b
Pramod <Pramod@DESKTOP-HD79JGD>
2022-04-06 22:07:25 +0530

    commite2

---
+++ JJJ/src/ss/so.java
@@ -0,0 +1,15 @@
+package ss;
+
+public class so {
+
+	public static void main(String[] args) {
+int x=23345;
+int length=0;
+ while(x>0) {
+	 x=x/10;
+	 length=length+1;
+ }
+System.out.println(length);
+	}
+
+}
